09ebe5f30b977805531fa452b3c87db85782ccc7
Наталья Чекмарева <natalyachekmareva@yandex.ru>
2023-05-08 19:25:15 +0400

    изменения 18.04

---
+++ cats/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from .models import Cat
+
+
+class CatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cat
+        # fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color', 'birth_year')
+        # fields = '__all__'
